064e340db012de3c22eb55c833947bb438494f91
young970 <yyww9@naver.com>
2023-02-07 00:26:18 +0900

    feat: Add 2231_분해합

--- 2231_분해합.py
+++ 2231_분해합.py
@@ -1,4 +1,14 @@
 import sys
-sys.stdin = open('input.txt', 'r')
-input = sys.stdin
+# sys.stdin = open('input.txt', 'r')
+input = sys.stdin.readline
 
+n = int(input())
+result = 0
+for i in range(1,n+1):
+    parts = list(map(int,str(i)))
+    result = i + sum(parts)
+    if result == n:
+        print(i)
+        break
+    if i == n:
+        print(0)
